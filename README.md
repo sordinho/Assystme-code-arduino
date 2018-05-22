4acb21c6d0c947f53ea21e618105ef90a4d968c9
sordinho <davi.sordi96@gmail.com>
2018-05-22 19:30:23 +0200

    First sketch. Water flow control ok. todo gas control

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Assystme-code-arduino
+
+This is the code running on Arduino for Sensor and Serial communication
+
+[Assystme-code](https://github.com/AmI-2018/Assystme-code)
+
+## Used sensors:
+#### Gas: 
+###### Pin Connection:
+Sensor | Arduino
+------- | --------
+ | 
+ | 
+ | 
+ | 
+#### Faucet: Ultrasonic sensor (HCSR04) to detect water flow
+###### Pin Connection:
+Sensor | Arduino
+------- | --------
+Vcc | 5V
+Echo | 10
+Trig | 9
+GND | GND
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,99 @@
 #include <Arduino.h>
 
-void setup() {
-    // put your setup code here, to run once:
+//Integrated led on Arduino board
+const int led = 13;
+// Interrupt pin for communcating problem to raspberry
+const int interruptFaucet = 2;
+const int interruptGas = 3;
+
+// Gas Sensor
+
+//Faucet Sensor (Ultrasonic sensor HCSR04)
+const int trigPin = 9;
+const int echoPin = 10;
+
+// Define variables
+long duration; // Used to measure the travel time of ultrasonic wave
+int distance;  // Used to get the distance (in our case we only need to know if we detect something near)
+
+// Function prototypes
+bool checkFlow();
+bool checkGas();
+
+void setup()
+{
+    // Arduino Setup function
+    pinMode(trigPin, OUTPUT);         // Sets the trigPin as an Output
+    pinMode(echoPin, INPUT);          // Sets the echoPin as an Input
+    pinMode(led, OUTPUT);             //Sets the integrated led as Output
+    pinMode(interruptFaucet, OUTPUT); // Sets the interrupt faucet pin as Output
+    pinMode(interruptGas, OUTPUT);    // Sets the interrupt gas pin as Output
+    Serial.begin(9600);               // Starts the serial communication
 }
 
-void loop() {
-    // put your main code here, to run repeatedly:
+void loop()
+{
+    // check if faucet left open
+    if (checkFlow())
+    {
+        //We detect open water
+        delay(5000); // 5 seconds delay to try to detect if water is still open
+        if (checkFlow())
+        {
+            // ok maybe we have a problem
+            digitalWrite(interruptFaucet, HIGH); // turn HIGH interrupt pin
+            delay(2000);                         // wait 2 seconds for rasp interrupt management
+            digitalWrite(interruptFaucet, LOW);  //restore at low faucet interrupt pin
+        }
+    }
+
+    // check for a gas leak
+    if (checkGas())
+    {
+        // Gas leak detected
+        digitalWrite(interruptGas, HIGH); // turn HIGH interrupt pin
+        delay(2000);
+        digitalWrite(interruptGas, LOW); // restore
+    }
+}
+
+/* Function to detect water flow
+ * Return True if water is detect, false if not
+*/
+bool checkFlow()
+{
+
+    // Clears the trigPin
+    digitalWrite(trigPin, LOW);
+
+    delayMicroseconds(5);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    // Reads from the echoPin the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
+
+    // Calculating the distance
+    distance = duration * 0.034 / 2;
+
+    // Distance measured above 5 cm ---> water flow
+    if (distance < 5)
+    {
+        digitalWrite(led, HIGH);
+        return true;
+    }
+    else
+    {
+        digitalWrite(led, LOW);
+        return false;
+    }
+}
+
+bool checkGas()
+{
+
+    // todo
+    return false;
 }
